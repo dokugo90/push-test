a6165add9b219d66dd818a31c9965d230584593f
dokugo90 <dokugo90@gmail.com>
2022-11-29 14:07:47 +0000

    React App

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Yayy! It Works</h1>
+    <button onClick={() => alert("It Worked")}>Click me</button>
+    </>
+  );
+}
+
+export default App;
